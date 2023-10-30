142b9f48c292a9d6064f449cad43a251271470c2
Антон Ткаченко <antontkacenko@MacBook-Air-Anton.local>
2023-10-30 18:55:42 +0200

    send emailJS form

--- src/components/Pages/Footer.jsx
+++ src/components/Pages/Footer.jsx
@@ -2,11 +2,44 @@ import Grid from "@mui/material/Grid";
 import "./footer.css";
 import EmailIcon from "@mui/icons-material/Email";
 import PhoneIcon from "@mui/icons-material/Phone";
+import React from "react";
+import emailjs from "emailjs-com";
 
 function Footer() {
   const phoneNumber = "+380503948155";
   const emailAddress = "atkachenko@gmail.com";
   const companyDescription = "Наші контакти";
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_kdgfocc",
+        "template_713i9v7",
+        e.target,
+        "fmoGGolBajMSGggZs"
+      )
+      .then((result) => {
+        console.log("Email sent successfully!", result.text);
+
+        e.target.reset();
+        const { name, email, message } = e.target.elements;
+
+        const data = {
+          from_name: name.value,
+          from_email: email.value,
+          message: message.value,
+        };
+
+        console.log("Имя:", name.value);
+        console.log("Email:", email.value);
+        console.log("Сообщение:", message.value);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error.text);
+      });
+  };
+
   return (
     <Grid container spacing={1} className="grid-container">
       <Grid item xs={12}>
@@ -35,6 +68,36 @@ function Footer() {
           </a>
         </div>
       </Grid>
+
+      <Grid item xs={12}>
+        <form onSubmit={sendEmail} className="contact-form">
+          <div className="form-group">
+            <input
+              type="text"
+              name="from_name"
+              placeholder="Ваше имя"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              name="from_email"
+              placeholder="Ваш Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <textarea name="message" placeholder="Сообщение" required />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="submit">
+              Отправить
+            </button>
+          </div>
+        </form>
+      </Grid>
+
       <Grid item xs={12}>
         <iframe
           title="Google Maps"
